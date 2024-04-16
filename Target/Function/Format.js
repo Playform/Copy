cfd89dc54ebb9291c2b810cb32e2afaeac76a43b
Nikola <nikola@playform.cloud>
2024-04-17 01:03:37 +0300

    squash!

--- Target/Function/Format.js
+++ Target/Function/Format.js
@@ -1,1 +1,1 @@
-var o=e=>t(e).filter(r=>r.from&&r.to).map(({from:r,to:a})=>({from:t(r),to:t(a)}));const{default:t}=await import("@Function/Ensure.js");export{t as Ensure,o as default};
+var o=e=>t(e).filter(r=>r.from&&r.to).map(({from:r,to:a})=>({from:t(r),to:t(a)}));const{default:t}=await import("./Ensure.js");export{t as Ensure,o as default};
