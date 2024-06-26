e63781b701bab9a890e8a8eeac1d39cdb4a5c5e2
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-06-26 03:33:10 +0300

    squash!

--- Target/Function/Format.d.ts
+++ Target/Function/Format.d.ts
@@ -9,4 +9,4 @@ declare const _default: (Asset: MaybeArray<AssetPair>) => {
 export default _default;
 import type AssetPair from "../Interface/AssetPair.js";
 import type MaybeArray from "../Type/MaybeArray.js";
-export declare const Ensure: <T>(item: MaybeArray<T>) => T[];
+export declare const Ensure: <T>(item: MaybeArray<T>) => Array<T>;
